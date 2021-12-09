623142dfaad25de60603ee74a0fe7073b66890b2
archieren <renjianpeng@gmail.com>
2021-12-09 16:46:35 +0800

    About LBR!

--- tensorgroup/models/networks/PointCloudTransformer.py
+++ tensorgroup/models/networks/PointCloudTransformer.py
@@ -14,11 +14,13 @@ from tensorflow.python.keras.engine.sequential import Sequential
 from .layers.msa import (MultiHeadSelfAttention as MSA)
 
 class LBR(Layer):
-    def __init__(self, channels):
+    def __init__(self, channels, repeats=2):
         super(LBR, self).__init__()
         # Dense作为Linear op
-        self.net = Sequential([ Dense(channels, use_bias=False), BatchNormalization(), ReLU(),
-                                Dense(channels, use_bias=False), BatchNormalization(), ReLU()])
+        self.net = Sequential([
+                            Sequential([ Dense(channels, use_bias=False), BatchNormalization(), ReLU()])
+                            for i in range(repeats)
+                            ])
 
 
     def call(self, x):
@@ -30,6 +32,7 @@ class LBR(Layer):
 class StackTransformerLayer(Layer):
     def __init__(self, channels=256, num_heads=1):
         super(StackTransformerLayer, self).__init__()
+        self.lbr = LBR(channels)
         self.msa1 = MSA(channels, num_heads)
         self.msa2 = MSA(channels, num_heads)
         self.msa3 = MSA(channels, num_heads)
@@ -37,17 +40,17 @@ class StackTransformerLayer(Layer):
         self.concat = Concatenate(axis=-1)
 
     def call(self, x):
+        x = self.lbr(x)
         x1 = self.msa1(x)
         x2 = self.msa2(x1)
         x3 = self.msa3(x2)
         x4 = self.msa4(x3)
-        return x4 # self.concat([x1, x2, x3, x4])
+        return self.concat([x1, x2, x3, x4])
     
     def get_config(self):
         return super().get_config()
 
 def point_cloud_transformer():
     inputs = Input(shape=(1000, 3))
-    lbr = LBR(channels=128)(inputs)
     outputs = StackTransformerLayer(channels=128)(inputs)
     return tf.keras.Model(inputs=inputs, outputs=outputs)
